380ea517436a39072784f97d4c6e15448295d19a
J. Colby Fisher <jcolbyfisher@gmail.com>
2022-11-23 10:39:20 -0600

    import processing sites from module

--- jobs/buildMenu.js
+++ jobs/buildMenu.js
@@ -1,99 +1,32 @@
+const logger = require('pino')();
 const { SimpleIntervalJob, AsyncTask } = require('toad-scheduler');
-const sqlite3 = require('sqlite3').verbose();
-const path = require('path');
 
-const feed = require('../meals/allSides');
-
-const { getSite, createSite, updateSite } = require('../daos/sites');
-const { getEntry, createEntry } = require('../daos/entries');
+const kitchen = require('../kitchens');
 
 const TASK_NAME = 'buildMenu';
 
 const task = new AsyncTask(
   TASK_NAME,
   () => {
-    console.log(TASK_NAME, 'starting task');
-
     return new Promise(async (resolve, reject) => {
-      const db = new sqlite3.Database(
-        path.join(__dirname, '../db/rss_feeder.sqlite')
-      );
-
       try {
-        const siteFeed = await feed();
-        const fetchedAt = new Date().getTime();
+        logger.info(`JOB: Starting AsyncTask ${TASK_NAME}`);
 
-        console.log(TASK_NAME, siteFeed.name, 'processing site');
+        await kitchen();
 
-        let siteCache = await getSite(db, siteFeed.name);
+        logger.info(`JOB: Finishing AsyncTask ${TASK_NAME}`);
 
-        if (siteCache) {
-          await updateSite(db, siteCache.id, fetchedAt);
-
-          siteCache.fetchedAt = fetchedAt;
-        } else {
-          siteCache = await createSite(
-            db,
-            siteFeed.name,
-            siteFeed.url,
-            siteFeed.icon,
-            fetchedAt
-          );
-
-          siteCache = await getSite(db, siteFeed.name);
-        }
-
-        console.log(
-          TASK_NAME,
-          siteFeed.name,
-          `processing 's ${siteFeed.entries.length} entries`
-        );
-
-        await Promise.all(
-          siteFeed.entries.map(async (entry) => {
-            try {
-              const existingEntry = await getEntry(
-                db,
-                siteCache.id,
-                entry.link
-              );
-
-              if (!existingEntry) {
-                await createEntry(
-                  db,
-                  siteCache.id,
-                  entry.title,
-                  entry.link,
-                  entry.content,
-                  entry.timestamp,
-                  fetchedAt
-                );
-              }
-
-              return true;
-            } catch (ex) {
-              console.error(TASK_NAME, ex, {
-                siteId: siteCache.id,
-                title: entry.title,
-                url: entry.link,
-              });
-            }
-          })
-        );
-
-        console.log(TASK_NAME, siteCache.name, 'job done');
-
-        return resolve();
+        return resolve(true);
       } catch (ex) {
-        console.error(TASK_NAME, ex, 'task runtime error');
         return reject(ex);
-      } finally {
-        db.close();
       }
     });
   },
   (ex) => {
-    console.error(TASK_NAME, ex, 'AsyncTask error');
+    logger.error(`JOB: AsyncTask ${TASK_NAME} has failed`);
+    logger.error(ex);
+
+    return false;
   }
 );
 
