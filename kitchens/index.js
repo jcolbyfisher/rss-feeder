a21b60fafaf7fadecc9f7bb85d119c6426b6041d
J. Colby Fisher <jcolbyfisher@gmail.com>
2022-11-23 10:51:32 -0600

    revert to promise to work with AsyncJob API correctly

--- kitchens/index.js
+++ kitchens/index.js
@@ -7,76 +7,77 @@ const { getEntry, createEntry } = require('../daos/entries');
 
 const feed = require('../meals/allSides');
 
-module.exports = async () => {
-  logger.info(`KITCHEN: Turning on the burners`);
+module.exports = () =>
+  new Promise(async (resolve, reject) => {
+    logger.info(`KITCHEN: Turning on the burners`);
 
-  const db = new sqlite3.Database(
-    path.join(__dirname, '../db/rss_feeder.sqlite')
-  );
+    const db = new sqlite3.Database(
+      path.join(__dirname, '../db/rss_feeder.sqlite')
+    );
 
-  try {
-    const siteFeed = await feed();
-    const fetchedAt = new Date().getTime();
+    try {
+      const siteFeed = await feed();
+      const fetchedAt = new Date().getTime();
 
-    logger.info(`KITCHEN: Cooking site: ${siteFeed.name}`);
+      logger.info(`KITCHEN: Cooking site: ${siteFeed.name}`);
 
-    let siteCache = await getSite(db, siteFeed.name);
+      let siteCache = await getSite(db, siteFeed.name);
 
-    if (siteCache) {
-      await updateSite(db, siteCache.id, fetchedAt);
+      if (siteCache) {
+        await updateSite(db, siteCache.id, fetchedAt);
 
-      siteCache.fetchedAt = fetchedAt;
-    } else {
-      siteCache = await createSite(
-        db,
-        siteFeed.name,
-        siteFeed.url,
-        siteFeed.icon,
-        fetchedAt
+        siteCache.fetchedAt = fetchedAt;
+      } else {
+        siteCache = await createSite(
+          db,
+          siteFeed.name,
+          siteFeed.url,
+          siteFeed.icon,
+          fetchedAt
+        );
+
+        siteCache = await getSite(db, siteFeed.name);
+      }
+
+      logger.info(
+        `KITCHEN: Processing ${siteFeed.name}'s ${siteFeed.entries.length} entries`
       );
 
-      siteCache = await getSite(db, siteFeed.name);
-    }
+      await Promise.all(
+        siteFeed.entries.map(async (entry) => {
+          try {
+            const existingEntry = await getEntry(db, siteCache.id, entry.link);
 
-    logger.info(
-      `KITCHEN: Processing ${siteFeed.name}'s ${siteFeed.entries.length} entries`
-    );
+            if (!existingEntry) {
+              await createEntry(
+                db,
+                siteCache.id,
+                entry.title,
+                entry.link,
+                entry.content,
+                entry.timestamp,
+                fetchedAt
+              );
+            }
 
-    await Promise.all(
-      siteFeed.entries.map(async (entry) => {
-        try {
-          const existingEntry = await getEntry(db, siteCache.id, entry.link);
-
-          if (!existingEntry) {
-            await createEntry(
-              db,
-              siteCache.id,
-              entry.title,
-              entry.link,
-              entry.content,
-              entry.timestamp,
-              fetchedAt
+            return true;
+          } catch (ex) {
+            logger.error(
+              `KITCHEN: Error processing ${siteFeed.name}'s entry: (title: ${entry.title}, url: ${entry.link})`
             );
           }
+        })
+      );
 
-          return true;
-        } catch (ex) {
-          logger.error(
-            `KITCHEN: Error processing ${siteFeed.name}'s entry: (title: ${entry.title}, url: ${entry.link})`
-          );
-        }
-      })
-    );
+      logger.info(`KITCHEN: Done processing ${siteFeed.name}`);
 
-    logger.info(`KITCHEN: Done processing ${siteFeed.name}`);
+      resolve(true);
+    } catch (ex) {
+      logger.error(`KITCHEN: Runtime error`);
+      logger.error(ex);
 
-    return true;
-  } catch (ex) {
-    logger.error(`KITCHEN: Runtime error`);
-    logger.error(ex);
-
-    throw ex;
-  } finally {
-    db.close();
-  }
-};
+      reject(ex);
+    } finally {
+      db.close();
+    }
+  });
