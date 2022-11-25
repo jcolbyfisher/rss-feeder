8f136ef3bcc960e07e235030bb515fef241b5823
J. Colby Fisher <jcolbyfisher@gmail.com>
2022-11-24 21:30:59 -0600

    helpful infos, linnk to atom feed

--- views/mealSummary.js
+++ views/mealSummary.js
@@ -3,9 +3,12 @@ const escape = require('html-escaper').escape;
 module.exports = (siteName, updatedAt, entries) => `
 <h1>${siteName}</h1>
 <p>Updated at: ${updatedAt.toLocaleDateString()} ${updatedAt.toLocaleTimeString()}</p>
+<p>Available feeds: <a href="${siteName}/atom">Atom</a></p>
+<p>Sample entries:</p>
 <ul>${entries
   .map(
     ({ title, url }) => `<li><a href="${escape(url)}">${escape(title)}</a></li>`
   )
   .join('')}
-</ul>`;
+</ul>
+`;
